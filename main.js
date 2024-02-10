19fcf181fbb69e246dc9d50a7e1cb4ab9adc4cc2
MartaGBayona <marta.g.bayona@outlook.es>
2024-02-10 16:12:15 +0100

    start the problem

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 export const fn = (/* argumentos de la función */) => {
    // Cuerpo de la función: Completa la lógica de la función aquí.
 };
+
+
+// precios empanadas
+
+const a = 12
+
+const b = 14
+
+const c = 16
+
+let ePollo = 1
+
+let eCarne = 0
+
+let eVegetal = 0
+
+
+if (eVegetal > ePollo) {
+   const resultado = ePollo - eVegetal
+   console.log(resultado)
+   if (resultado > 0) {
+
+   }
+}
+
