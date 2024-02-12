d29b6bde12dd7a3748ce5789a53faeb95ac0259e
MartaGBayona <marta.g.bayona@outlook.es>
2024-02-12 16:44:44 +0100

    declare variables and apply the 'throw error' filter

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-export const fn = (a, b, c) => {
-   
-
-   
-};
-
-const a = 9;
-const b = 10;
-const c = 11;
+const a = 0;
+const b = 0;
+const c = 0;
 
 const priceA = a * 12;
 const priceB = b * 14;
 const priceC = c * 16;
+
+const combiAC = (priceA + priceC)/2
+const combiAB = (priceA + priceB)/2
+const combiBC = (priceB + priceC)/2
+
+
+export const fn = (a, b, c) => {
+    if (((a + b + c) <= 39) && (a >= 0 && b >= 0 && c >= 0 ) && ((a + b + c) % 3 === 0)) {
+
+
+
+    } else {
+        "throw error"
+    }
+
+
+};
+
 
