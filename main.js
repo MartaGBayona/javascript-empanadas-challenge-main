ca2f9a6207078298ff1f111c1a5997ee0318aa40
MartaGBayona <marta.g.bayona@outlook.es>
2024-02-14 16:44:35 +0100

    renaming variables and adding comments to the code

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-// Variable declaration
 
-const a = 0;
-const b = 0;
-const c = 0;
-let combiEmpanadas = 0;
-
+// variable declaration
+let totalPie = 0;
 
 // Function
+export const fn = (pieA, pieB, pieC) => {
 
-export const fn = (a, b, c) => {
+    // error handling within the if conditional
+    if (((pieA + pieB + pieC) < 40) && ((pieA + pieB + pieC) >= 3) && (pieA >= 0 && pieB >= 0 && pieC >= 0) && (pieA + pieB + pieC) % 3 === 0) { 
 
-    if (((a + b + c) < 40) && ((a + b + c) >= 3) && (a >= 0 && b >= 0 && c >= 0) && (a + b + c) % 3 === 0) {
+        //calculate of the average of the products
+        totalPie = ((pieA * 12) + (pieB * 14) + (pieC * 16)) / 3
 
-        combiEmpanadas = ((a * 12) + (b * 14) + (c * 16)) / 3 // obtención de la media de la compra de los productos
-
-        while (combiEmpanadas % 1 !== 0) { // bucle que suma 0,1 hasta llegar al siguiente numero entero.
-            combiEmpanadas += 0.1;
-            combiEmpanadas = Math.round(combiEmpanadas * 10) / 10; // Corrección de errores con la suma de decimales.
+        // decimal correction with while loop
+        while (totalPie % 1 !== 0) { 
+            totalPie += 0.1;
+            //loop control with math.round and error correction with *10 / 10
+            totalPie = Math.round(totalPie * 10) / 10; 
         }
-
     } else {
-        throw error // evaluación de los erroes
+        throw error 
     }
-    return combiEmpanadas
+    return totalPie
 };
 
-fn(3, 3, 0)
+fn(11, 10, 9)
